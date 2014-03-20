b9cbc135c6ce8dec60595608bf3f1fcf9420f97c
Francis Brosnan Blázquez <francis@aspl.es>
2014-03-20 18:49:28 +0000

    valvula: * [fix] Updated valvula-mgr.py to fix several problems while detecting   postfix section configurations and reading postfix configurations at   different locations.

--- test/test_01.py
+++ test/test_01.py
@@ -36,6 +36,7 @@
 #          info@aspl.es - http://www.aspl.es/valvula
 
 import sys
+import os
 sys.path.append ("../server")
 
 # import module
@@ -79,13 +80,51 @@ def test_00 ():
     if value != "not_found":
         print "ERROR: expected not_found for postfix section but found: %s" % value
         return False
+
+    # check for single line decl
+    info ("Checking for single line decl examples..")
+    value = m._get_postfix_config_type ("smtpd_data_restrictions", "test_01.base")
+    if value != "single_line_decl":
+        print "ERROR: expected single_line_decl for postfix section but found: %s" % value
+        return False
     
+    return True
+
+def test_01_do_update_and_check (base_file, postfix_section, host, port, order):
+    import shutil
+    
+    # copy file
+    shutil.copyfile (base_file, "%s.cf" % base_file)
+    m._add_postfix_valvula_declaration (postfix_section, host, port, order, "%s.cf" % base_file)
+
+    if not os.path.exists ("%s.ref" % base_file):
+        print "ERROR: file %s.ref doesn't exist. Please run this command to check differences" % base_file
+        print "       >> diff %s %s.cf" % (base_file, base_file)
+        return False
+        
+    
+    if open ("%s.cf" % base_file).read () != open ("%s.ref" % base_file).read ():
+        print "ERROR: reference files %s.cf and %s.ref differs" % (base_file, base_file)
+        print "       run: "
+        print "       >> diff %s.cf %s.ref" % (base_file, base_file)
+        return False
+
     return True
 
 def test_01 ():
 
-    
+    # test single line first
+    if not test_01_do_update_and_check ("test_01.base", "smtpd_data_restrictions", "127.0.0.1", "3579", "first"):
+        return False
 
+    # test single line last
+    if not test_01_do_update_and_check ("test_01_last.base", "smtpd_data_restrictions", "127.0.0.1", "3579", "last"):
+        return False
+
+    # test multi line first
+    if not test_01_do_update_and_check ("test_01_multi_first.base", "smtpd_data_restrictions", "127.0.0.1", "3579", "first"):
+        return False
+    
     return True
 
 
