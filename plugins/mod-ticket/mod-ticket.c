255e8445fcbc5b370203e6cc3255e99e05e372a1
Francis Brosnan Blázquez <francis@aspl.es>
2014-06-06 11:18:28 +0000

    valvula: * [fix] More debug..

--- plugins/mod-ticket/mod-ticket.c
+++ plugins/mod-ticket/mod-ticket.c
@@ -514,7 +514,8 @@ ValvulaState ticket_process_request (ValvulaCtx        * _ctx,
 
 	
 	/* by default report return dunno */
-	printf ("..12..\n");
+	printf ("..12 (current_month_usage=%ld > month_limit=%ld, current_day_usage=%ld > day_limit=%ld) ..\n",
+		current_month_usage, month_limit, current_day_usage, day_limit);
 	return VALVULA_STATE_DUNNO;
 }
 
