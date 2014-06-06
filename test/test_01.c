135fe5130d7a60750178d7240a3b4a0f4fa8a9c3
acinom <acinom@7b0dd4af-71cf-42cc-8deb-632136f1e5f4>
2014-06-06 14:33:24 +0000

    valvula: * [fix] Updated regression test to report more information around 4.11

--- test/test_01.c
+++ test/test_01.c
@@ -1536,7 +1536,11 @@ axl_bool test_05 (void) {
 		"plain", "francis@aspl.es", NULL);
 
 	if (state != VALVULA_STATE_REJECT) {
-		printf ("ERROR (4.11): expected valvula state %d but found %d\n", VALVULA_STATE_REJECT, state);
+		/* check state to better configure the state */
+		if (state == VALVULA_STATE_DUNNO) 
+			printf ("ERROR (4.11): expected valvula state %d but found %d (should have received REJECT but received DUNNO)\n", VALVULA_STATE_REJECT, state);
+		else
+			printf ("ERROR (4.11): expected valvula state %d but found %d\n", VALVULA_STATE_REJECT, state);
 		return axl_false;
 	} /* end if */
 
