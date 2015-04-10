c3ff50240940a45fe4dc463450e3b7c833c216f4
Francis Brosnan Blázquez <francis@aspl.es>
2015-04-10 16:28:40 +0000

    valvula: * [fix] More updates to include svn revision into version. More   improvements on Centos packages...

--- buildrpm.sh
+++ buildrpm.sh
@@ -6,8 +6,11 @@ echo "Preparing sources"
 rm -rf rpm/SOURCES/*.tar.gz
 rm -rf rpm/BUILD/*
 rm -rf rpm/BUILDROOT/*
+find rpm/RPMS/ -type f -exec rm {} \;
+
+rm valvula-*.tar.gz
 make dist
-cp valvula-`cat VERSION`.tar.gz rpm/SOURCES/
+cp valvula-*.tar.gz rpm/SOURCES/
 
 echo "Calling to compile packages.."
 LANG=C rpmbuild -ba --define '_topdir /usr/src/valvula/rpm' rpm/SPECS/valvula.spec
