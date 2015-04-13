e8d8053007fd42d24431f48e9790f96a046ed1cd
acinom <acinom@7b0dd4af-71cf-42cc-8deb-632136f1e5f4>
2015-04-13 11:09:02 +0000

    valvula: * [fix] More improvements on centos packaging..

--- buildrpm.sh
+++ buildrpm.sh
@@ -16,6 +16,7 @@ echo "Calling to compile packages.."
 LANG=C rpmbuild -ba --define '_topdir /usr/src/valvula/rpm' rpm/SPECS/valvula.spec
 
 echo "Output ready at rpm/RPMS"
-find rpm/RPMS -type f
+find rpm/RPMS -type f -name '*.rpm' > rpm/RPMS/files
+cat rpm/RPMS/files
 
 
