e26354562b34ba7d7608332f143c7a37df7ce504
Francis Brosnan Blázquez <francis@aspl.es>
2016-05-06 13:01:24 +0200

    valvula: * [fix] Adding support to Ubuntu 16.04 xenial xerus

--- autogen.sh
+++ autogen.sh
@@ -59,7 +59,7 @@ touch NEWS README AUTHORS ChangeLog
 libtoolize --force;
 aclocal $ACLOCAL_FLAGS; 
 autoheader --warnings=error;
-automake --add-missing --Werror;
+automake --add-missing;
 autoconf --force --warnings=error;
 
 ./configure $@ --enable-maintainer-mode
