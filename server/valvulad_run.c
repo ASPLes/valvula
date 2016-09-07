3ce8f0539e22e3b34e2b95170272a1b1d6179c41
Francis Brosnan Blázquez <francis@aspl.es>
2016-09-07 20:27:34 +0200

    valvula: * [fix] Fixed password hiding (was not working)

--- server/valvulad_run.c
+++ server/valvulad_run.c
@@ -402,7 +402,7 @@ axl_bool valvulad_run_check_local_domains_config_detect_postfix_decl (ValvuladCt
 
 					if (key && decl) {
 						msg ("Declaration found: (%s) [%s] -> [%s]", 
-						     section, key, axl_cmp (decl, "password") ? "xxxxx" : decl );
+						     section, key, axl_cmp (key, "password") ? "xxxxx" : decl);
 						if (axl_cmp (section, "virtual_mailbox_domains")) {
 							if (axl_cmp (key, "user"))
 								ctx->ld_user = axl_strdup (decl);
