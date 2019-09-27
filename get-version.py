5be6176dc98f73827e0993b9cf2ab89e284cc34a
Francis Brosnan Blázquez <francis@aspl.es>
2019-09-27 08:50:18 +0200

    valvula: * [fix] Updated get-version.py and added LATEST-VERSION.

--- get-version.py
+++ get-version.py
@@ -3,24 +3,33 @@
 from core_admin_common import command, support
 import sys
 
-(status, info) = command.run ("LANG=C svn update . | grep 'At revision'")
+(osname, oslongname, osversion) = support.get_os ()
+release_name = osversion.split (" ")[1]
+no_github_com_access = ["lenny", "squeeze"]
+
+if release_name in no_github_com_access:
+    command.run ("cp -f LATEST-VERSION VERSION")
+    sys.exit (0)
+# end if
+
+(status, info) = command.run ("LANG=C svn update . | grep revision")
 if status:
     print "ERROR: unable to get subversion version: %s" % info
     sys.exit (-1)
 
 # get versision
-revision = support.get_int (info.split (" ")[2].replace (".", "").strip ())
+revision = info.split (" ")[2].replace (".", "").strip ()
 print "INFO: Revision found: %s" % revision
 
 version = open ("VERSION").read ().split (".b")[0].strip ()
-version = "%s.b%s" % (version, (revision + 1))
+version = "%s.b%s" % (version, revision)
 print "INFO: Updated vesion to: %s" % version
 
 open ("VERSION", "w").write ("%s\n" % version)
+open ("LATEST-VERSION", "w").write ("%s\n" % version)
 
 # also update Changelog
 command.run ("svn log > Changelog")
-
 
 
 
